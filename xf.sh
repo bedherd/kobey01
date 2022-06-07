6bbbe4d14986d27024a5297de8f6950ff72068fa
bedherd <107016884+bedherd@users.noreply.github.com>
2022-06-07 17:49:03 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "5f0bbb94-bdac-45f0-8257-063ff055f976",
                         "alterId": 0
                     }
                 ],
